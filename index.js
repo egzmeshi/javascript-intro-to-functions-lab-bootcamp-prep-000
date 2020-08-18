1b095c15f4cd517c7e89969ddeb5c7396babd0c0
Laiba Shah <lshah1@fordham.edu>
2020-08-18 17:02:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logWhisper(string) {
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase
 
+function logWhisper(string) {
+  console.log('Hello Grandma').toLowerCase()
 }
